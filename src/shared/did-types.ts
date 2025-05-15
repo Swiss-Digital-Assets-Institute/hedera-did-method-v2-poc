41c3cb4040eaf78ce0c674b3e30a25111361fb05
Jakub Sydor <jakub.sydor@leocode.com>
2025-05-15 10:44:43 +0200

    Add multi-controller PoC

--- src/shared/did-types.ts
+++ src/shared/did-types.ts
@@ -2,6 +2,28 @@ export interface VerificationMethod {
   id: string;
   type: string;
   controller: string;
+  publicKeyJwk?: {
+    kty: string;
+    crv?: string;
+    x?: string;
+    y?: string;
+    e?: string;
+    n?: string;
+    [key: string]: any;
+  };
+  publicKeyMultibase?: string;
+  publicKeyBase58?: string;
+  [key: string]: any;
+}
+
+export interface Service {
+  id: string;
+  type: string | string[];
+  serviceEndpoint:
+    | string
+    | string[]
+    | Record<string, any>
+    | Array<Record<string, any>>;
   [key: string]: any;
 }
 
@@ -11,10 +33,13 @@ export interface JsonLdDIDDocument {
   "@context": string | string[];
   id: string;
   controller: string | string[];
+  alsoKnownAs?: string[];
   verificationMethod?: VerificationMethod[];
   authentication?: KeyCapabilityMethod[];
   assertionMethod?: KeyCapabilityMethod[];
   keyAgreement?: KeyCapabilityMethod[];
   capabilityInvocation?: KeyCapabilityMethod[];
   capabilityDelegation?: KeyCapabilityMethod[];
+  service?: Service[];
+  [key: string]: any;
 }
